42ae1cedf950d1eb7ec05701e4f80ea54e73e919
SageCreations <sagecreations.ec@gmail.com>
2023-11-28 18:56:44 -0600

    Binary tree stuff from the book

---
+++ BinaryTreeStuff/node.java
@@ -0,0 +1,12 @@
+import java.io.*;
+
+public class node {
+    public int iData;
+    public double dData;
+    public node leftChild;
+    public node rightChild;
+    
+    public void DisplayNode() {
+        System.out.print("{" + iData + ", " + dData + "} ");
+    }
+}
