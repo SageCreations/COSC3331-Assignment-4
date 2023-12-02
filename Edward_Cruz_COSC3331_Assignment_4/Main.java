ac2e9049adb884d029675521b8e7d49f02d5ede1
sagecreations <sagecreations.ec@gmail.com>
2023-12-02 16:19:19 -0600

    see if this can be optimized with recursion functions

--- Edward_Cruz_COSC3331_Assignment_4/Main.java
+++ Edward_Cruz_COSC3331_Assignment_4/Main.java
@@ -16,10 +16,9 @@ public class Main {
 		for (int i = 0; i < dataArray.length; i++) {
 			randomDigit = rand.nextInt(899) + 100;
 			dataArray[i] = randomDigit;
-			//System.out.println(dataArray[i] + "->" + randomDigit%10);
 		}
 
-		// Declartion of QArray with size 10
+		// Declaration of QArray with size 10
 		LinkQueue[] QArray = new LinkQueue[10];
 		for (int i = 0; i < QArray.length; i++) {
 			QArray[i] = new LinkQueue();
@@ -27,56 +26,92 @@ public class Main {
 
 		// Iterate over dataArray
 		// Add numbers to their proper queue by basing it off of their 
-		// indivisual digit. 
-		for (int i = 0; i < dataArray.length; i++) {
-			// first round
-			// least sig digit (most right)
-			QArray[dataArray[i]%10].insert(dataArray[i]);
-			//System.out.println(element%10);
-		}
+		// individual digit.
+        for (int j : dataArray) {
+            QArray[j % 10].insert(j);
+        }
 		
-		// add the flag to the queues and display them.	
-		for (int i = 0; i < QArray.length; i++) { 
-			QArray[i].insert(-1);			
-			QArray[i].displayQueue();
-		}
+			
+
+		// add flag to populated arrays
+        for (LinkQueue linkQueue : QArray) {
+            if (!linkQueue.isEmpty()) {
+                linkQueue.insert(-1);
+            }
+            linkQueue.displayQueue();
+        }
 
 		System.out.println("\n");
 
-		// second round of sorting
-		for (int i = 0; i < QArray.length; i++) {
-			while (!QArray[i].isEmpty()) {	
-				int value = QArray[i].remove();
-				if (value == -1) {break;}
-				int num = value	% 100;				
-				QArray[num / 10].insert(value);
-			}
+		// second round of sorting.
+        for (int i = 0; i < QArray.length; i++) {
+            LinkQueue queue = QArray[i];
+            while (!queue.isEmpty()) {
+                int value = queue.remove();
 
-			// removes current flags and inserts it back at the end for the next rounds use.
-			QArray[i].insert(-1);
-		
-		}
+                if (value == -1) {
+                    break;
+                }
 
-		//display
-		for (int i =0; i<QArray.length; i++)
-			QArray[i].displayQueue();	
+                int num = value % 100;
+                int index = num / 10;
+				// add flag if it gets added to an unpopulated queue ahead of the current one, otherwise infinite loop
+                if (QArray[index].isEmpty() && index > i) {
+                    QArray[index].insert(-1);
+                    QArray[index].insert(value);
+                } else {
+                    QArray[index].insert(value);
+                }
+            }
+
+
+        }
+
+		// add flag to populated arrays
+        for (LinkQueue linkQueue : QArray) {
+            if (!linkQueue.isEmpty()) {
+                linkQueue.insert(-1);
+            }
+            linkQueue.displayQueue();
+        }
 
 		System.out.println("\n");
 
 		// third round
-		for (int i = 0; i < QArray.length; i++) {
-			while (!QArray[i].isEmpty()) {
-				int value = QArray[i].remove();
-				if (value == -1)
-					continue;
-				QArray[value / 100].insert(value);
-			}	
-		}
+        for (int i = 0; i < QArray.length; i++) {
+            LinkQueue linkQueue = QArray[i];
 
-		for (int i=0; i<QArray.length; i++)
-			QArray[i].displayQueue();	
+            while (!linkQueue.isEmpty()) {
+                int value = linkQueue.remove();
+
+                if (value == -1) {
+                    break;
+                } else {
+                    int index = value / 100;
+                    if (QArray[index].isEmpty() && index > i) {
+                        QArray[index].insert(-1);
+                        QArray[index].insert(value);
+                    } else {
+                        QArray[index].insert(value);
+                    }
+                }
+            }
+        }
+
+        for (LinkQueue linkQueue : QArray) linkQueue.displayQueue();
 	
 		System.out.println("\n");
+
+		int i = 0;
+		for (LinkQueue queue : QArray) {
+			while (!queue.isEmpty()) {
+				int data = queue.remove();
+				dataArray[i] = data;
+				System.out.println(dataArray[i++]); // i didnt want 
+			}
+		}
+
+		//for (int data : dataArray) System.out.println(data);
 	}
 
 
